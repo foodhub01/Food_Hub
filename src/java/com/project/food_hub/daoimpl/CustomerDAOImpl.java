5255f8e84942d0ff82213894c5407ce3a5695662
foodhub01 <rahulpatra257@gmail.com>
2018-02-24 18:25:37 +0530

    add
    add

--- src/java/com/project/food_hub/daoimpl/CustomerDAOImpl.java
+++ src/java/com/project/food_hub/daoimpl/CustomerDAOImpl.java
@@ -8,6 +8,7 @@ package com.project.food_hub.daoimpl;
 import com.project.food_hub.dao.CustomerDAO;
 import com.project.food_hub.entities.Customer;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,12 +19,14 @@ import java.util.logging.Logger;
  */
 public class CustomerDAOImpl implements CustomerDAO
 {
-Connection con = DBConnection.getConnection();
+
     @Override
+    
     public int addCustomer(Customer customer) {
-        
+        int count = 0;
         
     try {
+        Connection con = DBConnection.getConnection();
       PreparedStatement psmt = con.prepareStatement("Insert Customer(String customerName,String address,String emailId,int contactNo,String date,String password) Values(?,?,?,?,?,?)");
         psmt.setString(1,customer.getCustomerName());
         psmt.setString(2,customer.getAddress());
@@ -31,38 +34,109 @@ Connection con = DBConnection.getConnection();
         psmt.setInt(4,customer.getContactNo());
         psmt.setString(5,customer.getDate());
         psmt.setString(6,customer.getPassword());
+        count = psmt.executeUpdate();
     } catch (SQLException ex) {
         Logger.getLogger(CustomerDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
     }
-   return 0;
+   return count;
     }
 
     @Override
     public int deleteCustomer(int customerId) {
-    try {
         int count = 0;
+        try {
+        Connection con = DBConnection.getConnection();
         PreparedStatement psmt = con.prepareStatement("delete from customer where customerId(?)");
         psmt.setInt(1,customerId);
+        count = psmt.executeUpdate();
                 
                 } catch (SQLException ex) {
         Logger.getLogger(CustomerDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
     }
-    
+    return count;
     }
 
     @Override
     public List<Customer> getAllCustomer() {
-        return null;
+        List<Customer> customerList = null;
+        try {
+
+        Connection con = DBConnection.getConnection();
+        PreparedStatement psmt = con.prepareStatement("select * from Customer");
+        ResultSet resultSet = psmt.executeQuery();
+        customerList = new ArrayList<Customer>();
+        if(resultSet!=null){
+         while(resultSet.next()){
+           int customerId = resultSet.getInt(1);
+           String customerName = resultSet.getString(2);
+           String address = resultSet.getString(3);
+           String emailId = resultSet.getString(4);
+           int contactNo = resultSet.getInt(5);
+           String date = resultSet.getString(6);
+           String password = resultSet.getString(7);
+           Customer customer = new Customer(customerId, customerName, address, emailId, contactNo, date, password);
+           customerList.add(customer);
+         }   
+        }
+        } catch (SQLException ex) {
+        Logger.getLogger(CustomerDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+    }
+        return customerList;
     }
 
     @Override
     public Customer getCustomerByID(int customerId) {
-        return null;
+         List<Customer> customerList = null;
+        try {
+        Connection con = DBConnection.getConnection();
+        PreparedStatement psmt = con.prepareStatement("select * from Customer where customerId=?");
+         psmt.setInt(1,customerId);
+        ResultSet resultSet = psmt.executeQuery();
+        customerList = new ArrayList<Customer>();
+        if(resultSet!=null){
+         while(resultSet.next()){
+           int customerId = resultSet.getInt(1);
+           String customerName = resultSet.getString(2);
+           String address = resultSet.getString(3);
+           String emailId = resultSet.getString(4);
+           int contactNo = resultSet.getInt(5);
+           String date = resultSet.getString(6);
+           String password = resultSet.getString(7);
+           Customer customer = new Customer(customerId, customerName, address, emailId, contactNo, date, password);
+           customerList.add(customer);
+         }   
+        }
+        } catch (SQLException ex) {
+        Logger.getLogger(CustomerDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+    }
+        if(customerList.size()>0) return customerList.get(0);
+        else return null;
+        
     }
 
     @Override
-    public int updateCustomer(int customerId, Customer customer) {
-        return 0;
+    public int updateCustomer(int customerId,Customer customer){
+         int count = 0;
+        try {
+          Connection con = DBConnection.getConnection();
+        PreparedStatement psmt = con.prepareStatement("update customer set customerName=?,address=?,emailId=?,contactNo=?,date=?,password=? where customerId=?");
+        psmt.setString(1,customer.getCustomerName());
+        psmt.setString(2,customer.getAddress());
+        psmt.setString(3,customer.getEmailId());
+        psmt.setInt(4,customer.getContactNo());
+        psmt.setString(5,customer.getDate());
+        psmt.setString(6,customer.getPassword());
+        count =  psmt.executeUpdate();
+        } catch (SQLException ex) {
+        Logger.getLogger(CustomerDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+    }
+     
+    return count;
     }
     
+
 }
+
+
+        
+
--- src/java/com/project/food_hub/daoimpl/FloorBookingDAOImpl.java
+++ src/java/com/project/food_hub/daoimpl/FloorBookingDAOImpl.java
@@ -8,6 +8,7 @@ package com.project.food_hub.daoimpl;
 import com.project.food_hub.dao.FloorBookingDAO;
 import com.project.food_hub.entities.FloorBooking;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,16 +18,17 @@ import java.util.logging.Logger;
  * @author RaHuL
  */
 public class FloorBookingDAOImpl implements FloorBookingDAO{
-    Connection con = DBConnection.getConnection();
+    
 
     @Override
     public int addFloorBooking(FloorBooking floorbooking) {
         int count = 0;
         try {
+            Connection con = DBConnection.getConnection();
             PreparedStatement psmt = con.prepareStatement("Insert FloorBooking() into values(?,?)");
             psmt.setInt(1,floorbooking.getBookFloorId());
             psmt.setInt(2,floorbooking.getFloorNo());
-            psmt.executeUpdate();
+            count = psmt.executeUpdate();
         } catch (SQLException ex) {
             Logger.getLogger(FloorBookingDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -35,22 +37,83 @@ public class FloorBookingDAOImpl implements FloorBookingDAO{
 
     @Override
     public int deleteCustomer(int bookFloorId) {
-       return 0;
+        int count = 0;
+        try {
+            Connection con = DBConnection.getConnection();
+            PreparedStatement psmt = con.prepareStatement("delete from floorbooking where bookFloorId=?");
+            psmt.setInt(1,bookFloorId);
+            count = psmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(FloorBookingDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return count;
     }
 
     @Override
     public List<FloorBooking> getAllFloorBooking() {
-        return null;
+        List<FloorBooking> floorBookingList = null;
+        try {
+            Connection connection = DBConnection.getConnection(); 
+            PreparedStatement psmt = connection.prepareStatement("select * from FloorBooking");
+            ResultSet resultSet = psmt.executeQuery();
+            floorBookingList = new ArrayList<FloorBooking>();
+            if(resultSet!=null){
+                while(resultSet.next()){
+                    int floorBookId = resultSet.getInt(1);
+                    int floorNo = resultSet.getInt(2);
+                    int customerId = resultSet.getInt(3);
+                    FloorBooking floorBooking = new FloorBooking(floorBookId,floorNo,customerId);
+                    floorBookingList.add(floorBooking);
+                }
+            }
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(FloorBookingDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+     return floorBookingList;
     }
 
     @Override
     public FloorBooking getFloorBookingbyId(int bookFloorId) {
-        return null;
+           List<FloorBooking> floorBookingList = null;
+        try {
+            Connection connection = DBConnection.getConnection(); 
+            PreparedStatement psmt = connection.prepareStatement("select * from FloorBooking where bokkFloorId=?");
+            psmt.setInt(1, bookFloorId);
+            ResultSet resultSet = psmt.executeQuery();
+            floorBookingList = new ArrayList<FloorBooking>();
+            if(resultSet!=null){
+                while(resultSet.next()){
+                    int floorBookId = resultSet.getInt(1);
+                    int floorNo = resultSet.getInt(2);
+                    int customerId = resultSet.getInt(3);
+                    FloorBooking floorBooking = new FloorBooking(floorBookId,floorNo,customerId);
+                    floorBookingList.add(floorBooking);
+                }
+            }
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(FloorBookingDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        if(floorBookingList.size()>0) return floorBookingList.get(0);
+        else return null;
     }
 
     @Override
     public int updateFloorBooking(int bookfloorId, FloorBooking floorbooking) {
-        return 0;
+        int count = 0;
+        try {
+            Connection con = DBConnection.getConnection();
+            PreparedStatement psmt = con.prepareStatement("update floorbooking set floorNo=?,customerId=?");
+            psmt.setInt(1,floorbooking.getFloorNo());
+            psmt.setInt(2,floorbooking.getCustomerId());
+            count = psmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(FloorBookingDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return count;
     }
     
 }
