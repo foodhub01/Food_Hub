eb49bfbf199713311c08e02942702e378d798f4b
foodhub01 <36477247+foodhub01@users.noreply.github.com>
2018-02-24 18:33:18 +0530

    add
    add

--- src/java/com/project/food_hub/daoimpl/OrderDAOImpl.java
+++ src/java/com/project/food_hub/daoimpl/OrderDAOImpl.java
@@ -7,7 +7,11 @@ package com.project.food_hub.daoimpl;
 
 import com.project.food_hub.dao.OrderDAO;
 import com.project.food_hub.entities.Order;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -17,27 +21,115 @@ public class OrderDAOImpl implements OrderDAO {
 
     @Override
     public int addOrder(Order order) {
-
+        int count=0;
+        try {
+            Connection con = DBConnection.getConnection();
+            PreparedStatement psmt = con.prepareStatement("Insert order(int customerId,int productId,int employeeId,int quantity,int billNo,String date)values(?,?,?,?,?,?)");
+            psmt.setInt(1,order.getCustomerId());
+            psmt.setInt(2,order.getProductId());
+            psmt.setInt(3,order.getEmployeeId());
+            psmt.setInt(4,order.getQuantity());
+            psmt.setInt(5,order.getBillNo());
+            psmt.setString(6,order.getDate());
+            count=psmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(OrderDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return count;
     }
 
     @Override
     public int deleteOrder(int orderId) {
-
+        int count=0;
+        try {
+            Connection con = DBConnection.getConnection();
+            PreparedStatement psmt = con.prepareStatement("Delete from order where orderId=?");
+            psmt.setInt(1, orderId);
+            count=psmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(OrderDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return count;
     }
 
     @Override
     public List<Order> getAllOrder() {
-
+         List<Order> orderList=null;
+        try {
+           
+            Connection con= DBConnection.getConnection();
+            PreparedStatement psmt=con.prepareStatement("Select *from order");
+            ResultSet resultSet=psmt.executeQuery();
+            orderList= new ArrayList<Order>();
+            if(resultSet!=null){
+                //resultSet.first();
+                while(resultSet.next()){
+                    int customerId = resultSet.getInt(1);
+                    int productId = resultSet.getInt(2);
+                    int employeeId = resultSet.getInt(3);
+                    int quantity = resultSet.getInt(4);
+                    int billNo= resultSet.getInt(5);
+                    String date= resultSet.getString(6);
+                    Order order = new Order(customerId,productId,employeeId,quantity,billNo,date);
+                    orderList.add(order);
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(OrderDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            return orderList;
     }
 
     @Override
     public Order getOrderById(int orderId) {
+          List<Order> orderList=null;
+        try {
+           
+            Connection con= DBConnection.getConnection();
+            PreparedStatement psmt=con.prepareStatement("Select *from order where orderId=?");
+            psmt.setInt(1, orderId);
+            ResultSet resultSet=psmt.executeQuery();
+            orderList= new ArrayList<Order>();
+            if(resultSet!=null){
+                //resultSet.first();
+                while(resultSet.next()){
+                    int customerId = resultSet.getInt(1);
+                    int productId = resultSet.getInt(2);
+                    int employeeId = resultSet.getInt(3);
+                    int quantity = resultSet.getInt(4);
+                    int billNo= resultSet.getInt(5);
+                    String date= resultSet.getString(6);
+                    Order order = new Order(customerId,productId,employeeId,quantity,billNo,date);
+                    orderList.add(order);
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(OrderDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        if(orderList.size()>0)return orderList.get(0);
+        else return null;
 
     }
 
     @Override
     public int updateOrder(int orderId, Order order) {
-
-    }
+        int count=0;
+        try {
+            Connection con = DBConnection.getConnection();
+            PreparedStatement psmt = con.prepareStatement("Update order set customerId=?, productId=?,employeeId=?,quantity=?,billno=?,date=? where orderId=? ");
+            psmt.setInt(1,order.getCustomerId());
+            psmt.setInt(2,order.getProductId());
+            psmt.setInt(3,order.getEmployeeId());
+            psmt.setInt(4,order.getQuantity());
+            psmt.setInt(5,order.getBillNo());
+            psmt.setString(6,order.getDate());
+            psmt.setInt(7, orderId);
+            count=psmt.executeUpdate();
+       
+        } catch (SQLException ex) {
+            Logger.getLogger(OrderDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return count;
+    }       
     
 }
--- src/java/com/project/food_hub/daoimpl/TableDAOImpl.java
+++ src/java/com/project/food_hub/daoimpl/TableDAOImpl.java
@@ -6,8 +6,12 @@
 package com.project.food_hub.daoimpl;
 
 import com.project.food_hub.dao.TableDAO;
-import com.project.food_hub.entities.Table;
+import com.project.food_hub.entities.*;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -17,27 +21,104 @@ public class TableDAOImpl implements TableDAO {
 
     @Override
     public int addTable(Table table) {
-
+        int count=0;
+        try {
+            Connection con = DBConnection.getConnection();
+            PreparedStatement psmt=con.prepareStatement("Insert mytable(int capacity) values(?)");
+            psmt.setInt(1,table.getCapacity());
+            count=psmt.executeUpdate();
+                    
+                    
+                    } catch (SQLException ex) {
+            Logger.getLogger(TableDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+                
+        return count;
     }
 
     @Override
     public int deleteTable(int tableNo) {
-
+        int count=0;
+        try {
+            Connection con = DBConnection.getConnection();
+            PreparedStatement psmt=con.prepareStatement("Delete from mytable where tableno=?");
+            psmt.setInt(1, tableNo);
+            count=psmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(TableDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return count;
     }
 
     @Override
     public List<Table> getAllTable() {
-
+        List<Table> tableList = null;
+        try {
+            
+            
+            
+            Connection con=DBConnection.getConnection();
+            PreparedStatement psmt=con.prepareStatement("Select * from mytable");
+            ResultSet resultSet=psmt.executeQuery();
+            tableList = new ArrayList<Table>();
+            if(resultSet!=null){
+                //resultSet.first();
+                while(resultSet.next()){
+                    int tableno = resultSet.getInt(1);
+                    int capacity = resultSet.getInt(2);
+                    Table table = new Table(tableno,capacity);
+                    tableList.add(table);
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(TableDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return tableList;
     }
 
     @Override
     public Table getTableById(int tableNo) {
-
+        List<Table> tableList = null;
+        try {
+            
+            
+            
+            Connection con=DBConnection.getConnection();
+            PreparedStatement psmt=con.prepareStatement("Select * from mytable where tableno=?");
+            psmt.setInt(1, tableNo);
+            ResultSet resultSet=psmt.executeQuery();
+            tableList = new ArrayList<Table>();
+            if(resultSet!=null){
+                //resultSet.first();
+                while(resultSet.next()){
+                    int tableno = resultSet.getInt(1);
+                    int capacity = resultSet.getInt(2);
+                    Table table = new Table(tableno,capacity);
+                    tableList.add(table);
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(TableDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        if(tableList.size()>0) return tableList.get(0);
+        else return null;
+        
     }
 
     @Override
     public int updateTable(int tableNo, Table table) {
-
+        int count=0;
+        try {
+            Connection con = DBConnection.getConnection();
+            PreparedStatement psmt=con.prepareStatement("Update mytable set capacity=? where tableno=?;");
+            psmt.setInt(1,table.getCapacity() );
+            psmt.setInt(2, tableNo);
+            count = psmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(TableDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return count;
     }
     
 }
